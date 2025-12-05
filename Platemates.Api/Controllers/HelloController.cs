b74aa82782f579eaf9c11f4354c1a90fba4f00b4
Nora Silfver <nora.silfver@gmail.com>
2025-12-05 15:26:41 +0100

    Add initial project structure

---
+++ Platemates.Api/Controllers/HelloController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Platemates.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HelloController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<string> GetHelloWorld()
+    {
+        var hello = "Hello World";
+
+        return Ok(hello);
+    }
+
+    // [HttpGet]
+    // [Route("ConnectionString")]
+    // public ActionResult<string> GetConnectionString()
+    // {
+    //     var builder = WebApplication.CreateBuilder();
+
+    //     var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+    //     if (connectionString == null)
+    //         return NotFound();
+
+    //     return Ok(connectionString);
+    // }
+}
