eabd238f01b14e6876eff27f9d2234ca1f4d194b
Nora Silfver <nora.silfver@gmail.com>
2025-12-07 14:56:32 +0100

    Add TestController with GET and POST endpoints for TestEntity

---
+++ Platemates.Api/Controllers/TestController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Platemates.Api.Data;
+using Platemates.Api.Models;
+
+namespace Platemates.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TestController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public TestController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: api/test
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<TestEntity>>> GetAll()
+    {
+        return await _context.TestEntities.ToListAsync();
+    }
+
+    // POST: api/test
+    [HttpPost]
+    public async Task<ActionResult<TestEntity>> Create([FromBody] string name)
+    {
+        var entity = new TestEntity { Name = name };
+        _context.TestEntities.Add(entity);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetAll), new { id = entity.Id }, entity);
+    }
+}
