f44fc5a7a60981f19035259031a12f0718c91e87
Nora Silfver <nora.silfver@gmail.com>
2025-12-10 16:53:59 +0100

    feat: Add migration to update models and remove TestEntities table

--- Platemates.Infrastructure/Migrations/20251210152734_UpdateModels.Designer.cs
+++ Platemates.Infrastructure/Migrations/20251210152734_UpdateModels.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Platemates.Infrastructure.Persistence;
@@ -11,9 +12,11 @@ using Platemates.Infrastructure.Persistence;
 namespace Platemates.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251210152734_UpdateModels")]
+    partial class UpdateModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
